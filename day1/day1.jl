d103d0e88b0ccf1d43d81518fc31a856fbce0e0b
Heath Raftery <heath@empirical.ee>
2022-12-01 16:35:12 +1100

    Finish day1

--- day1/day1.jl
+++ day1/day1.jl
@@ -1,1 +1,15 @@
-println("Hello World")
+using DelimitedFiles;
+
+inp = Matrix{Int}[]
+
+open("input.txt", "r") do io
+    while !eof(io)
+        str = readuntil(io, "\n\n")
+        push!(inp, readdlm(IOBuffer(str)))
+    end
+end
+
+println(maximum(sum.(inp)))
+
+s = sort(sum.(inp), rev=true)
+println(sum(s[1:3]))
