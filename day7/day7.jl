1c11a7acd35b6d4545c4aabbe666cc095e4d6ba4
Heath Raftery <heath@empirical.ee>
2022-12-07 21:26:40 +1100

    Day 7 done, but ugly.

--- day7/day7.jl
+++ day7/day7.jl
@@ -1,17 +1,57 @@
-struct File
-    size :: Int
+mutable struct Node
+    isFile :: Bool
+    parent :: Union{Node, Nothing}
     name :: String
+    size :: Union{Int, Missing}
+    children :: Vector{Node}
+end
+Dir(p, n) =     Node(false, p, n, missing, [])
+File(p, n, s) = Node(true, p, n, s, [])
+
+function pp(node :: Node, descend=false, depth=0)
+    print(repeat(' ', depth*2))
+    print("- " * node.name * " ")
+    println("(" *
+            (node.isFile ? "file" : "dir") *
+            (ismissing(node.size) ? "" : ", size=" * string(node.size)) *
+            ")")
+        
+    if descend
+        for child in node.children
+            pp(child, descend, depth+1)
+        end
+    end
 end
 
-struct Dir
-    parent :: Union{Dir, Nothing}
-    name :: String
-    dirs :: Vector{Dir}
-    files :: Vector{File}
+function calculatesize(node :: Node)
+    if !ismissing(node.size)
+        return node.size # nothing more to be done
+    end
+
+    if isempty(node.children)
+        node.size = 0
+    else
+        node.size = sum([calculatesize(n) for n in node.children])
+    end
+    
+    return node.size
 end
 
-tree = open("input.test") do f
-    root = Dir(nothing, "/", [], [])
+function dfs(node :: Node, pred :: Function, matches :: Vector{Node} = Node[])
+    if(pred(node))
+        push!(matches, node)
+    end
+
+    for child in node.children
+        dfs(child, pred, matches)
+    end
+
+    return matches
+end
+
+
+tree = open("input.txt") do f
+    root = Dir(nothing, "/")
 
     currentDir = root
 
@@ -23,41 +63,56 @@ tree = open("input.test") do f
             elseif dirname == ".."
                 currentDir = currentDir.parent
             else
-                idx = findfirst(d -> d.name == dirname, currentDir.dirs)
+                idx = findfirst(c -> c.name == dirname, currentDir.children)
                 if isnothing(idx)
-                    newDir = Dir(currentDir, dirname, [], [])
-                    push!(currentDir.dirs, newDir)
+                    newDir = Dir(currentDir, dirname)
+                    push!(currentDir.children, newDir)
                     currentDir = newDir
                 else
-                    currentDir = currentDir.dirs[idx]
+                    currentDir = currentDir.children[idx]
                 end
             end
         elseif startswith(l, "\$ ls")
-            # No particular action since listings are self-evident
+            # No particular action since listing lines are self-evident
         else
             size,name = split(l)
-            if size == "dir"
-                push!(currentDir.dirs, Dir(currentDir, name, [], []))
-            else
-                push!(currentDir.files, File(parse(Int, size), name))
-            end
+            push!(currentDir.children,
+                  size == "dir" ? Dir(currentDir, name)
+                                : File(currentDir, name, parse(Int, size)))
         end
     end
 
     root
 end
 
-function pp(node :: Union{Dir, File}, depth=0)
-    print(repeat(' ',depth*2))
-    print("- " * node.name * " ")
-    if isa(node, File)
-        println("(file, size=" * string(node.size) * ")")
-    else
-        println("(dir)")
-        for child in #sort([node.dirs; node.files], by = c->c.name)
-            pp(child, depth+1)
-        end
-    end
+
+#pp(tree, true)
+#println("")
+
+calculatesize(tree)
+
+#pp(tree, true)
+#println("")
+
+dirs_no_bigger_than_100000 = dfs(tree, n -> !n.isFile && n.size <= 100000)
+
+for n in dirs_no_bigger_than_100000
+    pp(n)
 end
 
-pp(tree)
+println("\nPart 1: " * string(sum([n.size for n in dirs_no_bigger_than_100000])))
+println("")
+
+const DISK_SIZE = 70000000
+const TARGET_AVAIL_SIZE = 30000000
+
+deletion_size_target = TARGET_AVAIL_SIZE - (DISK_SIZE - tree.size)
+
+dirs_bigger_than_target = dfs(tree, n -> !n.isFile && n.size >= deletion_size_target)
+
+for n in dirs_bigger_than_target
+    pp(n)
+end
+
+target_dir = first(sort(dirs_bigger_than_target, by=n->n.size))
+println("\nPart 2: " * string(target_dir.size))
